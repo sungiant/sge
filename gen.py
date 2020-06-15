e11e3e458a368ae957e4913ea6b5e5500342da5f
Ash Pook <sungiant@live.co.uk>
2020-06-15 16:52:10 +0100

    wip

--- gen.py
+++ gen.py
@@ -9,13 +9,6 @@ projects_dir = os.path.join (script_dir, 'projects')
 if not os.path.isdir (projects_dir):
     os.mkdir (projects_dir)
 
-if platform.system() == 'nt':
-    subprocess.call (['cmake', '-G', 'Visual Studio 16 2019', build_dir], cwd = projects_dir)
-elif os.name == 'posix':
-    subprocess.call (['cmake', '-G', 'Xcode', build_dir], cwd = projects_dir)
-else:
-    subprocess.call (['cmake', build_dir], cwd = projects_dir)
-
 system = platform.system()
 
 if system == 'Windows':
