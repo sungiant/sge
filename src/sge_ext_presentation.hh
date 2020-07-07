d8ec563fb85290ff128bd76d4c0424b5226e3616
Ash Pook <sungiant@live.co.uk>
2020-07-07 22:00:46 +0100

    wip

--- src/sge_ext_presentation.hh
+++ src/sge_ext_presentation.hh
@@ -12,6 +12,19 @@ public:
     presentation (runtime::api& z) : runtime::system (z, "Presentation") {}
 
     virtual void managed_debug_ui () override {
+
+        ImGui::Text ("Compute target size: %dx%d",
+            sge.system__get_state_int (sge::runtime::system_int_state::compute_width),
+            sge.system__get_state_int (sge::runtime::system_int_state::compute_height));
+    
+        ImGui::Text ("Screen size: %dx%d",
+            sge.system__get_state_int (sge::runtime::system_int_state::screen_width),
+            sge.system__get_state_int (sge::runtime::system_int_state::screen_height));
+        
+        ImGui::Text ("Maximum resolution: %dx%d",
+            sge.system__get_state_int (sge::runtime::system_int_state::display_width),
+            sge.system__get_state_int (sge::runtime::system_int_state::display_height));
+        
         if (ImGui::Button ("Toggle Fullscreen")) {
             sge.system__toggle_state_bool (sge::runtime::system_bool_state::fullscreen);
         }
